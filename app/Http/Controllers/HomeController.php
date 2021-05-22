152fa02d069e1086e7dc39c99f8352246a061233
darman1725 <darmansaragih1701@gmail.com>
2021-05-22 22:30:37 +0700

    Mengedit Function Index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+        return view('home', ['user' => $user]);
     }
 }
