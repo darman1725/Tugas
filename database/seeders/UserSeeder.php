44a8462134eef70c7503b73c0131896f7c064f73
darman1725 <darmansaragih1701@gmail.com>
2021-05-22 14:14:40 +0700

    Mengedit File UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+             'email' => 'admin@admin.com',
+             'password' => Hash::make('password'),
+        ]);
     }
 }
