a13fdd1bc01ad24136f81ad02667e55d4eae18e5
darman1725 <darmansaragih1701@gmail.com>
2021-05-22 22:38:49 +0700

    Mengedit File Index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,6 @@ class HomeController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('home', ['user' => $user]);
+        return view('home',['user' => $user]);
     }
 }
