7805b274b2401611c07df1cf6f533232e0b28a36
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-02 02:25:38 +0700

    Membuat request validation repository

---
+++ app/Http/Requests/RepositoryRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RepositoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        if ($this->isMethod('POST')) {
+            return [
+                'judul' => 'required',
+                'jenis' => 'required',
+                'abstrak' => 'required',
+                'file' => 'required|max:5120|mimes:pdf',
+            ];
+        } else {
+            return [
+                'judul' => 'required',
+                'jenis' => 'required',
+                'abstrak' => 'required',
+            ];
+        }
+    }
+
+    public function messages()
+    {
+        return [
+            'judul.required' => 'Judul dokumen tidak boleh kosong',
+            'jenis.required' => 'Jenis dokumen tidak boleh kosong',
+            'abstrak.required' => 'Abstrak dokumen tidak boleh kosong',
+            'file.required' => 'File dokumen tidak boleh kosong',
+            'file.max' => 'Ukuran file dokumen maksimal 5 MB',
+            'file.mimes' => 'File harus berupa PDF',
+        ];
+    }
+}
