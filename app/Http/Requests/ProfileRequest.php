37937fdece68cbe5bb6142a5be7c916eae37d4c4
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-01 13:07:10 +0700

    Membuat Validasi form untuk data profile

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => 'required',
+            'name' => 'required',
+            'nim' => 'required|numeric',
+            'email' => 'required|email',
+            'prodi' => 'required',
+            'jurusan' => 'required',
+            'phone' => 'required',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'username.required' => 'Username harus diisi',
+            'nim.required' => 'NIM harus diisi',
+            'name.required' => 'Nama harus diisi',
+            'email.required' => 'Email harus diisi',
+            'email.email' => 'Email tidak valid',
+            'phone.required' => 'Nomor HP harus diisi',
+            'prodi.required' => 'Program studi harus diisi',
+            'jurusan.required' => 'Jurusan harus diisi',
+        ];
+    }
+}
