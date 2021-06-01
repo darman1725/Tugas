af0ca237ea84b890e5531c762923c8143f05d3c6
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-02 02:29:28 +0700

    Membuat halaman detail repository

---
+++ resources/views/repository/show.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.main')
+
+@section('title', 'Data Repository')
+
+@section('content')
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-2">
+        <h1 class="h3 text-gray-800">Data Repository</h1>
+        <a href="{{route('repository.index')}}" class="btn btn-sm btn-outline-scondary shadow-sm"><i class="fas fa-arrow-left fa-sm"></i> Kembali</a>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-6">
+            <div class="card shadow my-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Detail Dokumen</h6>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Nama Penerbit</div>
+                                <div class="col-md-8">{{ Auth::user()->name }}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Program Studi</div>
+                                <div class="col-md-8">{{ Auth::user()->prodi }}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Jurusan</div>
+                                <div class="col-md-8">{{ Auth::user()->jurusan }}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Judul Dokumen</div>
+                                <div class="col-md-8">{{$data->judul}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Jenis Dokumen</div>
+                                <div class="col-md-8">{{$data->jenis}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Tanggal Terbit</div>
+                                <div class="col-md-8">{{ $data->created_at->isoFormat('dddd, D MMMM Y') }}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4 font-weight-bold">Abstrak Dokumen</div>
+                                <div class="col-md-8">{{$data->abstrak}}</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card shadow my-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Preview Dokumen</h6>
+                </div>
+                <div class="card-body">
+                    <object data="{{asset('dokumen/' . $data->file)}}" type="application/pdf" class="w-100" height="600">
+                        <iframe src="https://docs.google.com/viewer?url={{asset('dokumen/' . $data->file)}}&embedded=true"></iframe>
+                    </object>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+@endsection
