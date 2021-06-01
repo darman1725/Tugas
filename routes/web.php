800c25b875eb1caf2a0e0104e568a7f901b25582
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-01 12:27:36 +0700

    Menambahkan middleware auth pada route

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
