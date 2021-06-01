f0bd0ab700e3c1d7dfee62c7edaa6f3af728e0ae
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-02 02:25:26 +0700

    Membuat Repository Model

---
+++ app/Models/Repository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Repository extends Model
+{
+    use HasFactory;
+
+    protected $table = 'repository';
+
+    protected $fillable = [
+        'judul',
+        'jenis',
+        'abstrak',
+        'komentar',
+        'file',
+    ];
+}
