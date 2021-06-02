fe661362cc7754ee6f353f4cbeadaed2b3dd56d2
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-02 19:44:16 +0700

    Menambahkan fitur preview dokumen sebelum proses penyimpanan atau submit

--- resources/views/repository/create.blade.php
+++ resources/views/repository/create.blade.php
@@ -63,8 +63,101 @@
                 </div>
             </div>
         </div>
+        <div class="col-md-6" style="display: none;" id="card-view">
+            <div class="card shadow my-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Preview Dokumen</h6>
+                </div>
+                <div class="card-body">
+                    <div class="alert alert-danger" role="alert" id="alert-view" style="display: none;"></div>
+                    <div id="loading" style="display: none">
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        Proses preview dokumen..
+                    </div>
+                    <div style="height:600px;overflow-y:scroll;" id="pdfViewer" class="w-100"></div>
+                </div>
+            </div>
+        </div>
     </div>
 
 </div>
 <!-- /.container-fluid -->
+@endsection
+
+@section('script')
+
+<script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
+
+<script>
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+    // The workerSrc property shall be specified.
+    pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+    $("#file").on("change", function(e) {
+        $('#card-view').show()
+        $('#alert-view').hide();
+        var file = e.target.files[0]
+        if (file.type == "application/pdf") {
+            var fileReader = new FileReader();
+            fileReader.onload = function() {
+                var pdfData = new Uint8Array(this.result);
+                // Using DocumentInitParameters object to load binary data.
+                var loadingTask = pdfjsLib.getDocument({
+                    data: pdfData
+                });
+                $('#loading').show()
+                loadingTask.promise.then(function(pdf) {
+                    // var pageNumber = 1;
+                    $('#loading').hide()
+                    for (var i = 1; i <= pdf.numPages; i++) {
+                        renderPage(i, pdf);
+                    }
+                }, function(reason) {
+                    // PDF loading error
+                    $('#loading').hide()
+                    $('#alert-view').show();
+                    $('#alert-view').html('Preview dokumen gagal. Silahkan ulangi input dokumen.');
+                });
+            };
+            fileReader.readAsArrayBuffer(file);
+        }
+    });
+
+    function renderPage(pageNumber, pdf) {
+        pdf.getPage(pageNumber).then(function(page) {
+            var canvasId = 'pdf-viewer-' + pageNumber;
+            $('#pdfViewer').append($('<canvas />', {
+                'id': canvasId
+                , 'class': 'w-100'
+            , }));
+            var canvas = document.getElementById(canvasId);
+
+            var scale = 1.5;
+            var viewport = page.getViewport({
+                scale: scale
+            });
+
+            // // Prepare canvas using PDF page dimensions
+            // var canvas = $("#pdfViewer")[0];
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context
+                , viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.promise.then(function() {
+                // console.log('Page rendered');
+            });
+        });
+
+    }
+
+</script>
+
 @endsection
--- resources/views/repository/edit.blade.php
+++ resources/views/repository/edit.blade.php
@@ -71,9 +71,17 @@
                     <h6 class="m-0 font-weight-bold text-primary">Preview Dokumen</h6>
                 </div>
                 <div class="card-body">
-                    <object data="{{asset('dokumen/' . $data->file)}}" type="application/pdf" class="w-100" height="600">
+                    <object data="{{asset('dokumen/' . $data->file)}}" type="application/pdf" class="w-100" height="600" id="viewerPdf">
                         <iframe src="https://docs.google.com/viewer?url={{asset('dokumen/' . $data->file)}}&embedded=true"></iframe>
                     </object>
+                    <div class="alert alert-danger" role="alert" id="alert-view" style="display: none;"></div>
+                    <div id="loading" style="display: none">
+                        <div class="spinner-border text-primary" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        Proses preview dokumen..
+                    </div>
+                    <div style="height:600px;overflow-y:scroll;display: none;" id="pdfViewer" class="w-100"></div>
                 </div>
             </div>
         </div>
@@ -81,4 +89,82 @@
 
 </div>
 <!-- /.container-fluid -->
+@endsection
+
+@section('script')
+
+<script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
+
+<script>
+    var pdfjsLib = window['pdfjs-dist/build/pdf'];
+    // The workerSrc property shall be specified.
+    pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+    $("#file").on("change", function(e) {
+        $('#viewerPdf').hide()
+        $('#pdfViewer').show()
+        $('#alert-view').hide();
+
+        var file = e.target.files[0]
+        if (file.type == "application/pdf") {
+            var fileReader = new FileReader();
+            fileReader.onload = function() {
+                var pdfData = new Uint8Array(this.result);
+                // Using DocumentInitParameters object to load binary data.
+                var loadingTask = pdfjsLib.getDocument({
+                    data: pdfData
+                });
+                $('#loading').show()
+                loadingTask.promise.then(function(pdf) {
+                    // var pageNumber = 1;
+                    $('#loading').hide()
+                    for (var i = 1; i <= pdf.numPages; i++) {
+                        renderPage(i, pdf);
+                    }
+                }, function(reason) {
+                    // PDF loading error
+                    $('#loading').hide()
+                    $('#alert-view').show();
+                    $('#alert-view').html('Preview dokumen gagal. Silahkan ulangi input dokumen.');
+                });
+            };
+            fileReader.readAsArrayBuffer(file);
+        }
+    });
+
+    function renderPage(pageNumber, pdf) {
+        pdf.getPage(pageNumber).then(function(page) {
+            var canvasId = 'pdf-viewer-' + pageNumber;
+            $('#pdfViewer').append($('<canvas />', {
+                'id': canvasId
+                , 'class': 'w-100'
+            , }));
+            var canvas = document.getElementById(canvasId);
+
+            var scale = 1.5;
+            var viewport = page.getViewport({
+                scale: scale
+            });
+
+            // // Prepare canvas using PDF page dimensions
+            // var canvas = $("#pdfViewer")[0];
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context
+                , viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.promise.then(function() {
+                // console.log('Page rendered');
+            });
+        });
+
+    }
+
+</script>
+
 @endsection
