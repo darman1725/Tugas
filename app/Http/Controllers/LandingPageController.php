03141b83c0811b25fd8927a92257fde60f007a59
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-07 13:24:32 +0700

    menambahkan route controller landing page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -12,4 +12,21 @@ class LandingPageController extends Controller
         $repo = Repository::with('user', 'tipe')->limit(4)->get();
         return view('lp.index', compact('repo'));
     }
+    public function repository()
+    {
+        $repo = Repository::with('user', 'tipe')->paginate(10);
+        return view('lp.repo', compact('repo'));
+    }
+    public function about()
+    {
+        return view('lp.about');
+    }
+    public function contact()
+    {
+        return view('lp.contact');
+    }
+    public function faq()
+    {
+        return view('lp.faq');
+    }
 }
