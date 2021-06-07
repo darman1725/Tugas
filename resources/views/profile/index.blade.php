a437e9fb2203fa25d63b91cdcc3a9277cecbf533
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-07 10:59:56 +0700

    Mendefiniskan data profil pada halaman profil

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.main')
+
+@section('title', 'Data Profile')
+
+@section('content')
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Data Profile</h1>
+    <div class="row">
+        <div class="col-md-8">
+            @if (Session::has('success'))
+            <div class="alert alert-success" role="alert">
+                {{Session::get('success')}}
+            </div>
+            @endif
+
+            <div class="card shadow my-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Data Profile</h6>
+                </div>
+                <div class="card-body">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col"></div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">Username</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->username}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">NIM</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->nim}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">Nama Lengkap</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->name}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">Email</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->email}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">Nomor HP</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->phone}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">Jurusan</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->jurusan}}</div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-md-4">Program Studi</div>
+                                <div class="col-md-6 font-weight-bold">{{Auth::user()->prodi}}</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+@endsection
