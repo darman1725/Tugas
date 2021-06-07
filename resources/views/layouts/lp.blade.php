8869fc7e8763cfdc403846b4af7e8d26fef52ec6
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-07 12:58:43 +0700

    menambahkan footer

--- resources/views/layouts/lp.blade.php
+++ resources/views/layouts/lp.blade.php
@@ -70,6 +70,16 @@
 
     @yield('content')
 
+    <footer class="bg-dark">
+        <div class="container">
+            <div class="row">
+                <div class="col text-center py-3 text-light">
+                    &copy; 2021. All right reversed.
+                </div>
+            </div>
+        </div>
+    </footer>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
