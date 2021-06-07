45b260e8646fc6544d3291fd9f55f399eeba48b3
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-07 13:25:48 +0700

    Membuat halaman contact

---
+++ resources/views/lp/contact.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.lp')
+
+@section('title', 'Contact Us')
+
+@section('content')
+
+<section class="my-5" style="min-height: 80vh;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mb-4 text-center">
+                <h3>Contact Us</h3>
+            </div>
+            <div class="col-12 mb-4">
+                <iframe src="https://www.google.com/maps/d/embed?mid=1MtzkdcMs_pChMkasMWzH0f_wKq0&hl=en" class="w-100" height="480"></iframe>
+            </div>
+            <div class="col-md-12">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, voluptas harum aliquid iste iusto blanditiis neque modi repellat ullam cum reiciendis sunt minima laudantium quia molestiae commodi eaque mollitia quae, non obcaecati labore. Earum corporis, dolorum libero architecto nobis, accusamus et aut doloribus aliquam numquam sunt magnam id sapiente porro.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
