c7f62738775e44da959ee0a84ce67c1ab401a403
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-07 13:25:11 +0700

    Edit route menu halaman utama

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="shortcut icon" href="{{asset('img/polinema.png')}}" type="image/x-icon">
 
     <title>@yield('title') - Repository App</title>
 
