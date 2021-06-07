c39746880cf2f86a2b27e413f96c6a4c56dc7855
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-08 05:30:45 +0700

    Menambahkan link lupa password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,9 +44,9 @@
                                     </button>
                                 </form>
                                 <hr>
-                                {{-- <div class="text-center">
-                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                </div> --}}
+                                <div class="text-center">
+                                    <a class="small" href="{{route('password.request')}}">Forgot Password?</a>
+                                </div>
                                 <div class="text-center">
                                     <a class="small" href="{{route('register')}}">Create an Account!</a>
                                 </div>
