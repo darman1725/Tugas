d69ae1662e306bfd13d1ff12613778822bea0707
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-09 02:59:31 +0700

    menambahakan controller pencarian repo

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -14,9 +14,28 @@ class LandingPageController extends Controller
     }
     public function repository()
     {
-        $repo = Repository::with('user', 'tipe')->paginate(10);
+        $repo = Repository::with('user', 'tipe')->paginate(2);
         return view('lp.repo', compact('repo'));
     }
+    public function detailRepository($id)
+    {
+        $repo = Repository::with('user', 'tipe')->findOrFail($id);
+        return view('lp.detail-repo', compact('repo'));
+    }
+
+    public function search(Request $request)
+    {
+        if ($request->keywords) {
+            $key = $request->keywords;
+            $repo = Repository::whereYear('created_at', $key)
+                ->orWhere('judul', 'like', "%" . $key . "%")
+                ->with('user', 'tipe')->paginate(1);
+            $repo->appends(['keywords' => $key]);
+            return view('lp.search-repo', compact('repo'));
+        }
+        return redirect()->route('lp.repo');
+    }
+
     public function about()
     {
         return view('lp.about');
@@ -28,5 +47,9 @@ class LandingPageController extends Controller
     public function faq()
     {
         return view('lp.faq');
+    }
+    public function policy()
+    {
+        return view('lp.policy');
     }
 }
