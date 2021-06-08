59c04259c3aae10bc870420a0e79bfc049b3efdc
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-09 03:00:29 +0700

    Membuat halaman policy

---
+++ resources/views/lp/policy.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.lp')
+
+@section('title', 'Policy')
+
+@section('content')
+
+<section class="my-5" style="min-height: 80vh;">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mb-4 text-center">
+                <h3>Policy</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
