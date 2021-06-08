b36296bc03c71e261233b06d4c9296975fa67420
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-09 03:44:06 +0700

    update halaman faq

--- resources/views/lp/faq.blade.php
+++ resources/views/lp/faq.blade.php
@@ -13,38 +13,38 @@
             @if ($data->isEmpty())
             <div class="col-12">
                 <div class="alert alert-danger">
-                    Data repository masih kosong.
+                    Data faq masih kosong.
                 </div>
             </div>
             @else
-           
-    @foreach ($data as $item)
-    <div class="col-12">
-        <div class="accordion" id="accordionExample{{$item->id}}">
-            <div class="card" id="heading{{$item->id}}">
-                <div class="card-header">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse{{$item->id}}" aria-expanded="true" aria-controls="collapse{{$item->id}}">
-                            {{$item->title}}
-                        </button>
-                    </h2>
-                </div>
 
-                <div id="collapse{{$item->id}}" class="collapse" aria-labelledby="heading{{$item->id}}" data-parent="#accordionExample{{$item->id}}">
-                    <div class="card-body">
-                      {{$item->content}}
+            @foreach ($data as $item)
+            <div class="col-12">
+                <div class="accordion" id="accordionExample{{$item->id}}">
+                    <div class="card" id="heading{{$item->id}}">
+                        <div class="card-header">
+                            <h2 class="mb-0">
+                                <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapse{{$item->id}}" aria-expanded="true" aria-controls="collapse{{$item->id}}">
+                                    {{$item->title}}
+                                </button>
+                            </h2>
+                        </div>
+
+                        <div id="collapse{{$item->id}}" class="collapse" aria-labelledby="heading{{$item->id}}" data-parent="#accordionExample{{$item->id}}">
+                            <div class="card-body">
+                                {{$item->content}}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    @endforeach
+            @endforeach
             <div class="col-12 my-3 text-center">
                 {{$data->links('vendor.pagination.bootstrap-4')}}
-    </div>
-    @endif
-    </div>
+            </div>
+            @endif
+        </div>
     </div>
 </section>
 
