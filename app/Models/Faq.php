b2f900ca7a06ed68d966edc75903e1b1046b9bd5
Darman Saragih <darmansaragih1701@gmail.com>
2021-06-09 03:07:10 +0700

    Edit fillable model faq

--- app/Models/Faq.php
+++ app/Models/Faq.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Faq extends Model
 {
     use HasFactory;
+
+    protected $table = 'faqs';
+
+    protected $fillable = [
+        'title',
+        'content'
+    ];
 }
